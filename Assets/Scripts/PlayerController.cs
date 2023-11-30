a73a8a2e4430bd03226707daeb93e0c5f5d9ce64
Leandro Brás <shadowgaming.7@gmail.com>
2023-11-30 19:06:02 +0000

    Add a basic player controller script to move the player arround

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Stats")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    // Movement Controls
+    private InputAction movementInput;
+    private Vector2 movementVector => movementInput.ReadValue<Vector2>().normalized;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        movementInput = GetComponent<PlayerInput>().actions["Movement"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        rb.velocity = movementVector * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
